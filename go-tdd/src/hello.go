fc061cd641030ae22d1ed5bb28db7b379928299b
Ryosuke <piesuke0727@gmail.com>
2021-12-29 22:55:23 +0900

    refector: hello.go

--- go-tdd/src/hello.go
+++ go-tdd/src/hello.go
@@ -1,16 +1,39 @@
-// ToDo 立ち止まるな！ その他の要件
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+const spanishHelloPrefix = "Hola, "
+
+const frenchHelloPrefix = "Bonjour, "
+
+const spanish = "Spanish"
+
+const french = "French"
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+// (prefix string) は named return value。関数にある変数でreturnされるもの
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
